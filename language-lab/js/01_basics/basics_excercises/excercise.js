5b1f298f9a4060868a2d032be58d075d914de43e
YogurtFusion <aniketkumar33805@outlook.com>
2026-01-10 17:43:03 +0530

    basics

---
+++ language-lab/js/01_basics/basics_excercises/excercise.js
@@ -0,0 +1,11 @@
+let a = "10"
+let b = 10
+
+console.log(a==b);
+console.log(a===b);
+
+let price = "199"
+let newPrice = parseInt(price)
+console.log(typeof(newPrice));
+console.log(`+1 will be ${newPrice+1} `);
+
---
+++ language-lab/js/01_basics/dom/script.js
@@ -0,0 +1,7 @@
+"Initialzing Java Script"
+
+const head = document.createElement("h1")
+head.innerHTML="heyy"
+document.body.append(head)
+
+const para = document.createTextNode
