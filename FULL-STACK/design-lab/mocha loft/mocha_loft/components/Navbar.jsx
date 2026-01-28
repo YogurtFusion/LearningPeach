994d03eb7d236f32302a763c4b48993d023f60e4
YogurtFusion <aniketkumar33805@outlook.com>
2026-01-28 14:40:31 +0530

    inital push

---
+++ FULL-STACK/design-lab/mocha loft/mocha_loft/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav>
+        {/* logo */}
+        <div className="">
+            <img src="" alt="" />
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
