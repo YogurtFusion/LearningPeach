5790bdba02f00405dfbdf433bf4cee2049aab4ec
YogurtFusion <cocaniket2@gmail.com>
2026-02-04 08:58:13 +0530

    inital commit

--- FULL-STACK/zero-to-production/server/index.js
+++ FULL-STACK/zero-to-production/server/index.js
@@ -5,17 +5,25 @@ const app = express();
 
 app.use(express.json());
 
-app.use(cors())
+app.use(cors(
+    {
+        origin: [
+            "http://localhost:5173",
+            "http://localhost:5174",
+            "http://localhost:3000",
+            // add production url
+        ],
+        credentials: true,
+    }
+))
 
 
 // API route
 app.get("/api/message", (req, res)=>{
-    res.json({message: "Hello from chapple "})
+    res.json({message: "Hello from chapple server "})
 })
 
 const PORT = 4000
 app.listen(PORT, ()=>
 console.log(`server is running at http://localhost:${PORT}`))
 
-
-return prom
