1eb5ad92355cadbf8bf6902df3239ecc67d1a0d9
YogurtFusion <cocaniket2@gmail.com>
2026-03-01 12:55:53 +0530

    variables revise

---
+++ language-lab/js/01_basics/Revise/01_variables&data.js
@@ -0,0 +1,16 @@
+// assigning variables from $ signs
+
+// set of rules are syntax and container are called varables 
+
+let $ = "some srting "
+let $dollar = "some srting "
+
+console.log($, $dollar);
+
+let item = {
+    ph_no: "890232348923890",
+    marks: "23"
+
+}
+console.log(item.marks, item.ph_no, Object.keys(item))
+console.log(Object.keys(item));
