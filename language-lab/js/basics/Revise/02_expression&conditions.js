888687190ca78c539b499a3c139d04e5849d966d
YogurtFusion <cocaniket2@gmail.com>
2026-03-02 14:23:03 +0530

    dataypes and expressions

---
+++ language-lab/js/basics/Revise/02_expression&conditions.js
@@ -0,0 +1,14 @@
+// use logical operators to find out whether the the age of  a person lies between 10 & 20
+
+let x = 10
+
+let y = 20
+// let z = x && y
+let age = 13
+
+if(x <= age && age <=y ){
+    console.log("yes it lies between y and x ")
+}else{
+console.log("no you're a unc now or drink milk ");
+
+}
