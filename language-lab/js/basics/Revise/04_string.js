8904dbc48a59ce2f3f7a5f655c004586bed63a50
YogurtFusion <cocaniket2@gmail.com>
2026-03-03 15:03:39 +0530

    string completed

---
+++ language-lab/js/basics/Revise/04_string.js
@@ -0,0 +1,10 @@
+let name1 = "Harry 1 "
+let name2 = "Naman "
+let name3 = name1.concat( name2 , "YES")
+
+console.log(name3);
+
+let newName = name1.trim()
+let name34 = newName.concat( name2 , "YES")
+console.log(name34, name34[0, 1,2,3,4,5,6,8,3], name1[0], [1],[2],[3],[4],[5],[6],[8],[3]);
+
