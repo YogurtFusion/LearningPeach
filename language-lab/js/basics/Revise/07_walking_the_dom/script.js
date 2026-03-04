120a8877ea45ed937250d661bfc808a09510a0d8
YogurtFusion <cocaniket2@gmail.com>
2026-03-04 17:31:14 +0530

    walk of dom came  in the middle

--- language-lab/js/basics/Revise/07_walking_the_dom/script.js
+++ language-lab/js/basics/Revise/07_walking_the_dom/script.js
@@ -1,15 +1,15 @@
-document.querySelector("li").style.color = "red"
+document.querySelector("li").style.color = "red";
 
-let threeElement = document.getElementById("threeElements")
-threeElement.firstElementChild.style.color="green"
-threeElement.lastElementChild.style.color = "green"
+let threeElement = document.getElementById("threeElements");
+threeElement.firstElementChild.style.color = "green";
+threeElement.lastElementChild.style.color = "green";
 
 // write a js program to change all the color of the li in cyan
 
-document.querySelector("ul").style.color = "cyan"
+document.querySelector("ul").style.color = "cyan";
 
-let list = document.querySelectorAll("li")
+let list = document.querySelectorAll("li");
 
-list.forEach((li)=>{
-    li.style.backgroundColor="cyan"
-})
+list.forEach((li) => {
+  li.style.backgroundColor = "cyan";
+});
