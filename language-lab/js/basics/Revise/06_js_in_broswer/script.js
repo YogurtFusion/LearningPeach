e5b5de3a246feb67d13c676345343e72cf428440
YogurtFusion <cocaniket2@gmail.com>
2026-03-04 14:20:29 +0530

    dom bascis and ex is compelte

--- language-lab/js/basics/Revise/06_js_in_broswer/script.js
+++ language-lab/js/basics/Revise/06_js_in_broswer/script.js
@@ -44,58 +44,52 @@ btn2.addEventListener("click", () => {
   }
 });
 
-// In the previous question use console.error to log the error if the age is entered is negative 
+// In the previous question use console.error to log the error if the age is entered is negative
 
-let btn3 = document.getElementById("btn3")
+let btn3 = document.getElementById("btn3");
 
-btn3.addEventListener("click",()=>{
+btn3.addEventListener("click", () => {
+  let input = prompt("enter your age");
+  if (input < 0) {
+    console.error("your input is less then 0");
+  } else if (input >= 18) {
+    alert("yes you can drive");
+  } else {
+    alert("you can't drive");
+  }
+
+  let again = confirm("are you wanna see the prompt again ");
+  if (again) {
     let input = prompt("enter your age");
-    if(input<0){
-        console.error("your input is less then 0")
+    if (input >= 18) {
+      alert("yes you can drive");
+    } else {
+      alert("no you can't drive");
     }
-    else if(input>=18) {
-        alert("yes you can drive")
-        
-    }
-    
-    else{
-        alert("you can't drive")
-    }
-    
-    let again = confirm("are you wanna see the prompt again ")
-    if(again){
-        let input = prompt("enter your age");
-        if(input>=18){
-            alert("yes you can drive")
-        }else{
-            alert("no you can't drive")
-        }
-    }
-})
+  }
+});
 
 // write  a program to change the url to google.com (redirection ) if user enter a number greater then 4
-let btn4 = document.getElementById("btn4")
+let btn4 = document.getElementById("btn4");
 
-btn4.addEventListener("click", ()=>{
-    let input = prompt("enter your number")
-    if(input>4){
-        window.location.href = "https://google.com"
-        
-    }
-})
-
+btn4.addEventListener("click", () => {
+  let input = prompt("enter your number");
+  if (input > 4) {
+    window.location.href = "https://google.com";
+  }
+});
 
 // change the background color of the page to yellow red or any color based on user input through prompt
 
-let userInput= document.getElementById("input")
+let userInput = document.getElementById("input");
 
-let button = document.getElementById("button")
-button.addEventListener("click",()=>{
-    document.body.style.backgroundColor=userInput.value
-})
+let button = document.getElementById("button");
+button.addEventListener("click", () => {
+  document.body.style.backgroundColor = userInput.value;
+});
 
-userInput.addEventListener("keydown", (e)=>{
-    if(e.key==="Enter"){
-        document.body.style.backgroundColor=userInput.value
-    }
-})
+userInput.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    document.body.style.backgroundColor = userInput.value;
+  }
+});
