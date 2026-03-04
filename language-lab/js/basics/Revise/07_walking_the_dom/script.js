ff59fcafb3752dac3bbfd63230475ba638d77a77
YogurtFusion <cocaniket2@gmail.com>
2026-03-04 17:29:02 +0530

    walk of dom came  in the middle

---
+++ language-lab/js/basics/Revise/07_walking_the_dom/script.js
@@ -0,0 +1,15 @@
+// document.querySelector("li").style.color = "red"
+
+let threeElement = document.getElementById("threeElements")
+threeElement.firstElementChild.style.color="green"
+threeElement.lastElementChild.style.color = "green"
+
+// write a js program to change all the color of the li in cyan
+
+// document.querySelector("nav").style.color = "cyan"
+
+let list = document.querySelectorAll("li")
+
+list.forEach((li)=>{
+    li.style.backgroundColor="cyan"
+})
