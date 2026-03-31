cb60af5dcdee394e9087b4c00f8a392f9145aa46
YogurtFusion <cocaniket2@gmail.com>
2026-03-31 17:26:31 +0530

    from and to and basics of gsap

---
+++ FRONTEND/gsap/script.js
@@ -0,0 +1,14 @@
+gsap.to("#box1", {
+  x: 1000,
+  duration: 2,
+  delay: 1,
+  rotate: 360,
+  backgroundColor: "#fff",
+  borderRadius: "100%"
+});
+
+gsap.from("#box2", {
+  x: 1200,
+  duration: 2,
+  delay: 1,
+});
