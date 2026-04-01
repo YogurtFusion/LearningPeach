4e9d9ded497a799b796ce72b870de9ffc17908df
YogurtFusion <cocaniket2@gmail.com>
2026-04-01 10:38:11 +0530

    timeline yoyo and repeat in gsap

--- FRONTEND/gsap/script.js
+++ FRONTEND/gsap/script.js
@@ -12,3 +12,65 @@ gsap.from("#box2", {
   duration: 2,
   delay: 1,
 });
+
+gsap.from("h1",{
+    opacity: 0,
+    duration: 1,
+    y:30,
+    delay: 1,
+    stagger: 0.3,
+})
+
+gsap.to("#Box",{
+    x:1200,
+    duration:1,
+    delay:1,
+    rotate: 360,
+    // infinity
+    repeat: -1,
+    // yoyo
+    yoyo: true,
+})
+
+gsap.to("#Box1",{
+    x:1500,
+    rotate: 360,
+    duration: 1.5,
+    delay:1
+})
+
+gsap.to("#Box2", {
+    x:1500,
+    backgroundColor:"lightyellow",
+    duration:1.5,
+    // box 1 duration + delay
+    delay:2.5
+
+})
+
+gsap.to("#Box3",{
+    x:1500,
+    scale: 0.5,
+    borderRadius:"50%",
+    duration:1.5,
+    // Box1 + Box2 : delay and duration = Box3 delay
+    delay: 4
+
+})
+
+// Timeline
+
+let tl =  gsap.timeline()
+
+tl.from("h2",{
+    y:-20,
+    opacity:0,
+    duration:1,
+    delay:0.5,
+})
+
+tl.from("h4",{
+    y:-20,
+    opacity:0,
+    duration:1
+})
