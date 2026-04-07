068640679f289fe9853d88087cae64db547f7f47
YogurtFusion <cocaniket2@gmail.com>
2026-04-07 13:20:37 +0530

    custom cursor with image and presnale exp

--- FRONTEND/gsap/project1/script.js
+++ FRONTEND/gsap/project1/script.js
@@ -1,10 +1,26 @@
-let main = document.querySelector("#main")
-let cursor = document.querySelector("#cursor")
+let main = document.querySelector("#main");
+let cursor = document.querySelector("#cursor");
+let imageDiv = document.querySelector("#image");
 
-main.addEventListener("mousemove", function(e){
+main.addEventListener("mousemove", function (e) {
+  gsap.to(cursor, {
+    x: e.x,
+    y: e.y,
+  });
+});
+
+imageDiv.addEventListener("mouseenter", function (e) {
+ cursor.innerHTML ="view more"
+    gsap.to(cursor, {
+    scale: 4,
+  });
+
+});
+
+
+imageDiv.addEventListener("mouseleave",function(e){
+    cursor.innerHTML =""
     gsap.to(cursor,{
-        x:e.x,
-        y:e.y,
-        duration:1,
+        scale:0,
     })
 })
