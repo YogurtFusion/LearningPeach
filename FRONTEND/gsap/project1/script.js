c40da86f44159e24cceec8b73497ca012e896c54
YogurtFusion <cocaniket2@gmail.com>
2026-04-07 12:39:38 +0530

    basic custom cursor

--- FRONTEND/gsap/project1/script.js
+++ FRONTEND/gsap/project1/script.js
@@ -1,2 +1,10 @@
 let main = document.querySelector("#main")
 let cursor = document.querySelector("#cursor")
+
+main.addEventListener("mousemove", function(e){
+    gsap.to(cursor,{
+        x:e.x,
+        y:e.y,
+        duration:1,
+    })
+})
