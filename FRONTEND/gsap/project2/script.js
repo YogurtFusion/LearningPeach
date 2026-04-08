c18d60d7e46188a8abba009f8859a36c7d551c95
YogurtFusion <cocaniket2@gmail.com>
2026-04-09 00:35:25 +0530

    little media queery

---
+++ FRONTEND/gsap/project2/script.js
@@ -0,0 +1,17 @@
+let tl = gsap.timeline()
+
+tl.to("#full",{
+    right:0,
+    duration:0.6,
+})
+
+tl.from("#full h4",{
+    x:150,
+    duration:0.7,
+    stagger:0.28,
+    opacity:0,
+})
+
+tl.from("#full i",{
+    opacity:0,
+})
