0e0cb6dc1f09e5b4c44882acab843cc12eb5376b
YogurtFusion <cocaniket2@gmail.com>
2026-04-12 12:14:26 +0530

    text break

--- FRONTEND/gsap/project3/script.js
+++ FRONTEND/gsap/project3/script.js
@@ -1,1 +1,17 @@
-let text
+function breakTheText() {
+  let h1 = document.querySelector("h1");
+  let h1text = h1.textContent;
+
+  let splittedText = h1text.split("");
+  console.log(splittedText);
+
+  let clutter = "";
+
+  splittedText.forEach(function (e) {
+    //   clutter = clutter + `<span>${e}</span>`;
+    clutter += `<span>${e}</span>`;
+    // clutter+=e
+  });
+  console.log(clutter);
+  h1.innerHTML = clutter;
+}
