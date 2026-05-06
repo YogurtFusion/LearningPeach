a2cde233117599c69312f73f0234ed5f69ea7f99
YogurtFusion <cocaniket2@gmail.com>
2026-05-07 05:24:33 +0530

    variables completed

---
+++ language-lab/js/basics/chai-js/basics/variables.js
@@ -0,0 +1,12 @@
+const accountId = 4902
+let accountEmail = "chapple@gmail.com" // we prefer let in modern world
+var accountPassword = "none of your business"
+accountCity = "astra"
+
+let accountState
+
+accountEmail = "Cheesepull@gmail.com"
+accountPassword = "again whay you cares"
+accountCity = "DholakPur"
+
+console.table([accountEmail,accountPassword,accountCity])
