83c5b7d196e3d5bf17f514911c575ccf424d4790
YogurtFusion <cocaniket2@gmail.com>
2026-05-26 22:29:51 +0530

    completed functions

--- language-lab/js/chai_or_js/03_basics/01_functions.js
+++ language-lab/js/chai_or_js/03_basics/01_functions.js
@@ -14,11 +14,13 @@ function sayMyName() {
 
 // }
 
-// sayMyName();
+sayMyName();
 
 //  sayMyName:  reference
 //  () is excecution
 // (number1, number3) now here it's called we're passing arguments
+
+
 
 // function addTwoNumbers(number1,number2){
 //     console.log(number1+number2);
@@ -33,11 +35,11 @@ function addTwoNumbers(number1, number2) {
 }
 
 const result = addTwoNumbers(3, 3); // addTwoNumbers(3,NaN)
-// console.log("Result: ", result);
+console.log("Result: ", result);
 
-// function loginUserMessage(username = "same") {       // here sam is default value now it can't be empty or undefined or what so ever  
+// function loginUserMessage(username = "same") {       // here sam is default value now it can't be empty or undefined or what so ever
 function loginUserMessage(username) {
-//   if (username === undefined) {
+  //   if (username === undefined) {
   //     console.log("YOO!!!.. 🥺👉👈 Please enter your user name YOO!!!... 🥺👉👈");
   //     return; // so that if it's true nothing else will be retured in this function
   //   }
@@ -50,7 +52,44 @@ function loginUserMessage(username) {
   return `${username} just logged in `;
 }
 
-// console.log(loginUserMessage("cheesey pizza") )
+console.log(loginUserMessage("cheesey pizza") )
 
 //loginUserMessage("cheesey pizza") // Will not print anything because console log is missing
-console.log(loginUserMessage()); // this will give undefined not NuN
+// console.log(loginUserMessage()); // this will give undefined not NuN
+
+function calculateCartPrice(val1, val2, ...num1) {
+  // it depends on the use case that '...' will be Rest operator or Spread operatory
+  return num1;
+}
+
+console.log(calculateCartPrice(200, 300, 400, 500, 2000)); // here "..." are Rest operator it means collect all the no. and give it to me ohhh yeahhhh
+
+const user = {
+  username: "chaple",
+  price: 199,
+};
+
+function handleObject(anyobject) {
+  console.log(
+    `username is ${anyobject.username} and price is ${anyobject.price}`,
+  );
+}
+
+// handleObject(user)
+
+handleObject({
+  //you can also pass direct object
+  username: "sam",
+  price: 399,
+});
+
+
+const myNewArray   = [200,300,400,500,600]
+
+function returnSecondValue(getArray){
+  return getArray[1]
+}
+
+console.log(`second value of myNewArray is ${returnSecondValue(myNewArray)}`)
+
+console.log(returnSecondValue([200,500,100,300]));// just like object you can make array on the spot  
--- language-lab/js/chai_or_js/03_basics/summary.js
+++ language-lab/js/chai_or_js/03_basics/summary.js
@@ -6,3 +6,9 @@ function loginUserMessage (username) {
 console.log(loginUserMessage("cheesey pizza") )
 console.log(loginUserMessage() )// this will give undefined not NuN
 
+function calculateCartPrice(val1, val2, ...num1){     // it depends on the use case that '...' will be Rest operator or Spread operatory   
+  return num1
+}
+
+console.log(calculateCartPrice(200,300,400,500,2000)); // here "..." are Rest operator it means collect all the no. and give it to me ohhh yeahhhh
+// first one will go in val 1 second in val 2 rest in ...num1 
