0d7d5a13caa43abe589dbad0ee4d4904f574128d
YogurtFusion <cocaniket2@gmail.com>
2026-05-26 15:59:30 +0530

    object part 2 completed

---
+++ language-lab/js/chai_or_js/02_basics/04_objects.js
@@ -0,0 +1,66 @@
+const tinderUser2 = new Object();
+const tinderUser = {};
+
+tinderUser.id = "id-01";
+tinderUser.name = "sammy";
+tinderUser.isLoggedIn = true;
+
+//console.log(tinderUser);
+
+const regularUser = {
+  email: "someone@gmail.com",
+  fullName: {
+    userFullName: {
+      firstNmae: "cheese",
+      lastname: "chaple",
+    },
+  },
+};
+
+// console.log(regularUser);
+// console.log("\n", regularUser.fullName);
+// console.log("\n", regularUser.fullName.userFullName);
+// console.log("\n", regularUser.fullName.userFullName.firstNmae); // if we wanna make sure that code don't break if it's don't exist then we use ?
+
+const obj1 = {1:"a",2:"b"}
+const obj2 = {3:"a",4:"b"}
+const obj3 = {5:"a",6:"b"}
+
+const obj4 = Object.assign({},obj1,obj2,obj3)
+// console.log(obj4);
+
+
+const obj5  = {...obj1,...obj2,...obj3}// we genrally use this one fo joing the obj it's also latest 
+console.log(obj5);
+
+
+// we get data/objects from database  in the array of objects
+const user = [
+    {
+        id:"id01",
+        email:"object-first@gmai.com"
+    },
+    {
+        id:"id02",
+        email:"object-second@email.com"
+    },
+    {
+        id:"id03",
+        email:"object-third@email.com"
+    },
+    {
+        id:"id04",
+        email:"object-fourth@email.com"
+    },
+]
+
+console.log(user[1].email)
+
+console.log(tinderUser);
+
+
+console.log(Object.keys(tinderUser));// most important for the project
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty("isLoggedIn"));
--- language-lab/js/chai_or_js/02_basics/summary.js
+++ language-lab/js/chai_or_js/02_basics/summary.js
@@ -12,3 +12,28 @@ console.log("B",myArr);
 const myn2 = myArr.splice(1,3)// slplice literally removes 
 console.log(myn2);
 console.log("c",myArr);
+
+
+// signleton-obj: made by constructor 
+// Object.create 
+const tinderUser = new Object()
+console.log(tinderUser);
+
+// following are the objects literals
+const tinderUser2 = {}
+console.log(tinderUser2);
+
+
+const mySym = Symbol("id1")
+
+const JsUser ={
+    name:"chaple",
+    "full name":"chple cheese",
+    // [Symbol("mySym2")]:"myKey1",// if you define symbol in obj you'll loose it's key everytime you call it  
+    [mySym]:"myKey2",
+}
+
+// console.log(JsUser.full name);
+console.log(JsUser["full name"]);// THIS IS THE better way to use objects
+// console.log(JsUser[Symbol("mySym")]); // if you don't define symbol outside it will say un defined 
+
