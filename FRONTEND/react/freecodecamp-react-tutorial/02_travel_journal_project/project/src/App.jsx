8e7400c40ebd326ef66610c9f6b6e36143390fea
YogurtFusion <cocaniket2@gmail.com>
2026-06-25 19:07:22 +0530

    completed basic app

---
+++ FRONTEND/react/freecodecamp-react-tutorial/02_travel_journal_project/project/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
+import heroImg from "./assets/hero.png";
+import "./App.css";
+import Header from "./components/Header";
+import Entry from "./components/Entry";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <Header/>
+      <Entry/>
+    </>
+  );
+}
+
+export default App;
---
+++ FRONTEND/react/freecodecamp-react-tutorial/02_travel_journal_project/project/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import Globe from "./Globe";
+import React from "react";
+const Header = () => {
+  return (
+    <header>
+      <Globe/>
+
+      <span className="header-text">my travel journal.</span>
+    </header>
+  );
+};
+
+export default Header;
