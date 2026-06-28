42540019e8f6c3154d2c2c5106d596788ad8e226
YogurtFusion <cocaniket2@gmail.com>
2026-06-28 09:25:29 +0530

    completed while loop

---
+++ language-lab/js/chai_or_js/05_iterations/02_while.js
@@ -0,0 +1,21 @@
+let index = 0;
+
+while (index <= 10) {
+  console.log(index);
+  index = index + 2;
+}
+
+let myArray = ["chaple", "cheese", "pizza"];
+
+let arr = 0;
+
+while (arr < myArray.length) {
+  console.log(`value of the array ${myArray[arr]}`);
+  arr = arr + 1;
+}
+
+let score = 11
+do{
+    console.log(`Score is ${score}`)
+    score ++
+}while(score<=10)
