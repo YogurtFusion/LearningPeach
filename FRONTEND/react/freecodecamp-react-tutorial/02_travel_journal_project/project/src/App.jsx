4debbaba94075d95a3b7f8daffedfc5e8270798b
YogurtFusion <cocaniket2@gmail.com>
2026-07-02 17:31:47 +0530

    coleted section 2

--- FRONTEND/react/freecodecamp-react-tutorial/02_travel_journal_project/project/src/App.jsx
+++ FRONTEND/react/freecodecamp-react-tutorial/02_travel_journal_project/project/src/App.jsx
@@ -12,28 +12,36 @@ function App() {
   const [count, setCount] = useState(0);
 
   const ninjaTurtles = ["Donatello", "Michaelangelo", "Rafael", "Leonardo"];
-  const EntryElement = data.map((item)=>{
-    return(
-      <Entry
-      key = {item.id}
-        countryName={item.country}
-        PlaceName={item.title}
-        img={{
-          src: `${item.img.src}` ,
-          alt: `${item.img.alt}`,
-        }}
-        dates={item.dates}
-        description={item.text}
-        link={item.googleMapsLink}
+  const EntryElement = data.map((entry) => {
+    console.log(data);
+
+    return (
+      <Entry key={entry.id}
+       {...entry}
+      //  entry={entry}
         />
-    )
-  })
+
+      // <Entry
+      // key = {item.id}
+      //   countryName={item.country}
+      //   PlaceName={item.title}
+      //   img={{
+      //     src: `${item.img.src}` ,
+      //     alt: `${item.img.alt}`,
+      //   }}
+      //   dates={item.dates}
+      //   description={item.text}
+      //   link={item.googleMapsLink}
+      //   />
+    );
+  });
+  console.log(Entry);
+
   return (
     <>
-
       <Header />
-{EntryElement}
-      <Entry
+      {EntryElement}
+      {/* <Entry
         countryName="JAPAN"
         PlaceName="Mount Fuji"
         img={{
@@ -43,8 +51,8 @@ function App() {
         dates="12 Jan, 2023 - 24 Jan, 2023"
         description="Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists."
         link="https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu"
-        />
-        {/* <main>{ninjaTurtles.map((item)=>(
+      /> */}
+      {/* <main>{ninjaTurtles.map((item)=>(
           <h2> {item} </h2>
         ))}</main> */}
     </>
--- FRONTEND/react/freecodecamp-react-tutorial/02_travel_journal_project/project/src/components/Entry.jsx
+++ FRONTEND/react/freecodecamp-react-tutorial/02_travel_journal_project/project/src/components/Entry.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 const Entry = (props) => {
   return (
+    // {...entry}
     <article className="card">
       <div className="card-picture">
         <img src={props.img.src} alt={props.img.alt} />
@@ -9,16 +10,67 @@ const Entry = (props) => {
         <div className="subhead">
           <img src="src\assets\marker.png" alt="marker icon" />
           <span>{props.countryName} </span>
-          <a className="subhead-link" href={props.link}>
+          <a className="subhead-link" href={props.googleMapsLink}>
             View on Google Maps
           </a>
         </div>
-        <h1>{props.PlaceName} </h1>
+        <h1>{props.title} </h1>
         <span>{props.dates} </span>
-        <p>{props.description} </p>
+        <p>{props.text} </p>
       </div>
     </article>
   );
 };
 
 export default Entry;
+
+
+// import React from "react";
+// const Entry = (props) => {
+//   return (
+//     <article className="card">
+//       <div className="card-picture">
+//         <img src={props.img.src} alt={props.entry.img.alt} />
+//       </div>
+//       <div className="card-body">
+//         <div className="subhead">
+//           <img src="src\assets\marker.png" alt="marker icon" />
+//           <span>{props.countryName} </span>
+//           <a className="subhead-link" href={props.googleMapsLink}>
+//             View on Google Maps
+//           </a>
+//         </div>
+//         <h1>{props.title} </h1>
+//         <span>{props.dates} </span>
+//         <p>{props.text} </p>
+//       </div>
+//     </article>
+//   );
+// };
+
+// export default Entry;
+
+// import React from "react";
+// const Entry = (props) => {
+//   return (
+//     <article className="card">
+//       <div className="card-picture">
+//         <img src={props.img.src} alt={props.img.alt} />
+//       </div>
+//       <div className="card-body">
+//         <div className="subhead">
+//           <img src="src\assets\marker.png" alt="marker icon" />
+//           <span>{props.countryName} </span>
+//           <a className="subhead-link" href={props.link}>
+//             View on Google Maps
+//           </a>
+//         </div>
+//         <h1>{props.PlaceName} </h1>
+//         <span>{props.dates} </span>
+//         <p>{props.description} </p>
+//       </div>
+//     </article>
+//   );
+// };
+
+// export default Entry;
