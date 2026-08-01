55d8f562d726b1587a71dc88bd1613a508cf1545
YogurtFusion <cocaniket2@gmail.com>
2026-08-01 23:24:29 +0530

    dom for each

--- language-lab/js/chai_or_js/06_dom/one.js
+++ language-lab/js/chai_or_js/06_dom/one.js
@@ -2,3 +2,16 @@ document.querySelector('input[type="password"]')
 document.querySelector('p:first-child')
 
 // node list is not a pure array
+
+// converting in the arrayy 
+let myarr 
+Array.from(myarr)
+
+const h2 = document.querySelectorAll(".mw-heading2")
+h2.forEach(function (h) {
+    h.style.color = "#f9dada";
+    h.style.backgroundColor = "peachpuff";
+    h.style.padding = "1rem";
+    h.style.borderRadius = "0.5rem"
+    h.innerText = "🧀"
+})
