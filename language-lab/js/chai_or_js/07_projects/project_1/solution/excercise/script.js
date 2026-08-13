a9205605fedce37feba7ec06e42b59da0e1fa2d8
YogurtFusion <cocaniket2@gmail.com>
2026-08-13 21:22:27 +0530

    added basic formating

--- language-lab/js/chai_or_js/07_projects/project_1/solution/excercise/script.js
+++ language-lab/js/chai_or_js/07_projects/project_1/solution/excercise/script.js
@@ -1,6 +1,6 @@
 const buttons = document.querySelectorAll(".button");
 
-const body = document.querySelector("body")
+const body = document.querySelector("body");
 
 buttons.forEach(function (button) {
   console.log(button);
