726321bca84dd96a50838e27da3cbc2d0d4d0c7a
YogurtFusion <cocaniket2@gmail.com>
2026-08-29 18:08:47 +0530

    BMI calculator task

--- language-lab/js/chai_or_js/07_projects/project_2/solution/script.js
+++ language-lab/js/chai_or_js/07_projects/project_2/solution/script.js
@@ -9,13 +9,18 @@ form.addEventListener("submit", function (e) {
 
   if (height === "" || height < "0" || isNaN(height)) {
     results.innerHTML = `Please give a valid height ${height} `;
-  }
- else if (weight === "" || weight < "0" || isNaN(weight)) {
+  } else if (weight === "" || weight < "0" || isNaN(weight)) {
     results.innerHTML = `Please give a valid weight ${weight} `;
-  }
-  else{
-    const bmi = (weight/((height*height)/10000)).toFixed(2)
+  } else {
+    const bmi = (weight / ((height * height) / 10000)).toFixed(2);
     // show the result
-    results.innerHTML = `<span>${bmi}</span>`
+
+    if (bmi < 18.6) {
+      results.innerHTML = `<span>${bmi}</span> <br> <p> you're under weight </p>`;
+    } else if (bmi >= 18.6 && bmi <= 24.9) {
+      results.innerHTML = `<span>${bmi}</span> <br> <p> you're Normal weight </p>`;
+    } else if (bmi > 24.9) {
+      results.innerHTML = `<span>${bmi}</span> <br> <p> you're over weight </p>`;
+    }
   }
 });
