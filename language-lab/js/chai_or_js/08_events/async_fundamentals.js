8ad870f49ba98fbbac4d397b6fd0a8c1421de6d6
YogurtFusion <cocaniket2@gmail.com>
2026-09-11 21:04:05 +0530

    added async file

--- language-lab/js/chai_or_js/08_events/async_fundamentals.js
+++ language-lab/js/chai_or_js/08_events/async_fundamentals.js
@@ -1,1 +1,1 @@
-//https://www.youtube.com/watch?v=zgt5oTD3rRc&list=PLu71SKxNbfoBuX3f4EOACle2y-tRC5Q37&index=37
+https://www.youtube.com/watch?v=zgt5oTD3rRc&list=PLu71SKxNbfoBuX3f4EOACle2y-tRC5Q37&index=38
